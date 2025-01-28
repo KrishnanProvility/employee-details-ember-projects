51af41703803ea83e574f50d1f394497bce0cbb6
KrishnanProvility <krishnan@provility.com>
2025-01-28 13:44:55 +0530

    Delete practice-ember directory

--- practice-ember/app/components/calculator.js
+++
@@ -1,7 +0,0 @@
-import Component from "@glimmer/component";
-
-export default class Calculator extends Component {
-  calculate(first, second, options) {
-    // ...
-  }
-}
--- practice-ember/app/helpers/substring.js
+++
@@ -1,3 +0,0 @@
-export default function substring(string, { start, end }) {
-  return string.substring(start || 0, end);
-}
